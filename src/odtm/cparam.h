44be29f99cd1dc6758ca14b890d5f4f67aa12d10
Prajeesh <prajeeshag@gmail.com>
2018-05-28 18:08:21 +0530

    Reproducibility check passed

--- src/odtm/cparam.h
+++ src/odtm/cparam.h
@@ -14,15 +14,10 @@ cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
                                   ! utilized in tracer.polar.F
                                   ! Default is 1.0
 
-!        alpha_rgm = 1.0  *      mod(loop,2)  
-!        alpha_rgt = 1.0  * (1 - mod(loop,2)) 
-!        beta_mldm = 1.0  *      mod(loop,2)  
-!        beta_mldt = 1.0  * (1 - mod(loop,2)) 
-
-        alpha_rgm = 1.0  
-        alpha_rgt = 1.0  
-        beta_mldm = 1.0  
-        beta_mldt = 1.0  
+        alpha_rgm = 0.0  *      mod(loop,2)  
+        alpha_rgt = 1.0  * (1 - mod(loop,2)) 
+        beta_mldm = 0.0  *      mod(loop,2)  
+        beta_mldt = 1.0  * (1 - mod(loop,2)) 
 
 				  ! coupling parameters
 				  ! alpha -> RG gets MYM
